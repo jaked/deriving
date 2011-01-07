9b20921a7e7ab9dfcd3b7600ab68da7dc32a482e
Gregoire Henry <henry@pps.jussieu.fr>
2011-01-07 04:22:01 +0100

    Fix in syntax extension: the expression "x < 1" didn't parse.
    Move the 'TRY' statement one step over.

--- syntax/extend.ml
+++ syntax/extend.ml
@@ -68,7 +68,7 @@ struct
   EXTEND Gram
   expr: LEVEL "simple"
   [
-  [e1 = TRY val_longident ; "<" ; t = ctyp; ">" ->
+  [ TRY [e1 = val_longident ; "<" ; t = ctyp; ">" ->
      match e1 with
        | <:ident< $uid:classname$ . $lid:methodname$ >> ->
          if not (Base.is_registered classname) then
@@ -92,7 +92,7 @@ struct
        | _ -> 
            fatal_error loc ("deriving: this looks a bit like a method application, but "
                             ^"the syntax is not valid");
-  ]];
+  ]]];
   END
   
 end
