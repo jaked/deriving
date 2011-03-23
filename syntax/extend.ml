a8a768812513cc13e9b20a87047ab6fd8eb4dcc8
Gregoire Henry <henry@pps.jussieu.fr>
2011-03-23 08:48:26 +0100

    Add syntax extension Class<typ> in module_expr.

--- syntax/extend.ml
+++ syntax/extend.ml
@@ -29,6 +29,23 @@ let derive_sig loc tdecls classname : Ast.sig_item =
   display_errors loc
     (fun name -> snd (Base.find name) (loc, tdecls)) classname
 
+let instantiate _loc t classname =
+
+  let module U = Type.Untranslate(struct let _loc = _loc end) in
+  let binding = Ast.TyDcl (_loc, "inline", [], t, []) in
+  let decls = display_errors _loc Type.Translate.decls binding in
+  if List.exists Base.contains_tvars_decl decls then
+    fatal_error _loc ("deriving: type variables cannot be used in `method' instantiations")
+  else
+    let tdecls = List.map U.decl decls in
+    let m = derive_str _loc decls classname in
+    <:module_expr< struct
+      type $list:tdecls$
+          $m$
+      include $uid:classname ^ "_inline"$
+    end >>
+
+
 module Deriving (S : Camlp4.Sig.Camlp4Syntax) =
 struct
 
@@ -45,23 +62,24 @@ struct
          if not (Base.is_registered classname) then
            fatal_error _loc ("deriving: "^ classname ^" is not a known `class'")
          else
-           let module U = Type.Untranslate(struct let _loc = _loc end) in
-           let binding = Ast.TyDcl (_loc, "inline", [], t, []) in
-           let decls = display_errors _loc Type.Translate.decls binding in
-             if List.exists Base.contains_tvars_decl decls then
-               fatal_error _loc ("deriving: type variables cannot be used in `method' instantiations")
-             else
-               let tdecls = List.map U.decl decls in
-               let m = derive_str _loc decls classname in
-                 <:expr< let module $uid:classname$ =
-                             struct
-                               type $list:tdecls$
-                               $m$
-                               include $uid:classname ^ "_inline"$
-                             end
-                          in $uid:classname$.$lid:methodname$ >>
+	   let m = instantiate _loc t classname in
+	   <:expr< let module $uid:classname$ = $m$
+                   in $uid:classname$.$lid:methodname$ >>
        | _ ->
            fatal_error _loc ("deriving: this looks a bit like a method application, but "
+                            ^"the syntax is not valid");
+  ]]];
+
+  module_expr: LEVEL "simple"
+  [
+  [ TRY [e1 = val_longident ; "<" ; t = ctyp; ">" ->
+     match e1 with
+       | <:ident< $uid:classname$ >> ->
+           if not (Base.is_registered classname) then
+             fatal_error _loc ("deriving: "^ classname ^" is not a known `class'");
+	   instantiate _loc t classname
+       | _ ->
+           fatal_error _loc ("deriving: this looks a bit like a class instantiation, but "
                             ^"the syntax is not valid");
   ]]];
   END
