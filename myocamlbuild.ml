1a3587d8dcebdd1b1bdd03395a9ff9a5e6fb653d
Hugo Heuzard <hugo.heuzard@gmail.com>
2014-02-11 13:42:02 +0100

    hack: Configure ocamlbuild to select the right ocamlfind

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -2,6 +2,17 @@
 (* OASIS_STOP *)
 
 let _ =
+
+  (* FIX START *)
+  (* fix needed by ocaml(build) 3.12.1(,4.00.1?) in order to pick the right ocamlfind *)
+
+  (* Fixed in later version with the following commit *)
+  (* ocamlbuild should look for ocamlfind on the path not in the root directory *)
+  (* https://github.com/ocaml/ocaml/commit/9d51dccfaebb2c3303ae0bb1d4f28fe6f8d10915 *)
+
+  let _ = Ocamlbuild_pack.Ocamlbuild_where.bindir := "/" in
+  (* FIX STOP *)
+
   Ocamlbuild_plugin.dispatch
     (fun hook ->
        dispatch_default hook;
